8ce6646f0591244001c2b4450b878f5265980c67
Piotr Przygoda <przygoda@gmail.com>
2019-02-25 22:28:48 +0100

    Finished mod 15.1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 'use strict';
-function Phone(brand, price, color) {
+function Phone(brand, price, color, screen, weight) {
 	this.brand = brand;
 	this.price = price;
-	this.color = color;
+    this.color = color;
+    this.screen = screen;
+    this.weight = weight;
+
 }
 Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
+
+var iPhone6S = new Phone("Apple", 2250, "silver", "6'", "149");
+var nokia = new Phone("Nokia", 1800, "black", "7'", "147" );
+var sony = new Phone("Sony", 2100, "black", "4,5'", "140");
+
+iPhone6S.printInfo();
+nokia.printInfo();
+sony.printInfo();
